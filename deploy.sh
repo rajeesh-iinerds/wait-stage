f3523e68151f79c27e69afff0661ee8cda1c97ec
Rajeesh Koroth <rajeesh.k@iinerds.com>
2017-12-14 20:00:52 +0530

    second commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-aws --profile gunbro cloudformation package --template-file template.yaml    --output-template-file serverless-output.yaml    --s3-bucket cicdsample.appcohesion.io
-aws --profile gunbro cloudformation deploy    --template-file serverless-output.yaml    --stack-name cicdcreatestages    --capabilities CAPABILITY_IAM
+aws --profile cct codepipeline create-pipeline --cli-input-json file://codepipeline.json
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+/**
+ * @author Rajeesh <rajeesh.k@iinerds.com>
+ * @version: 0.3
+ */
+
 'use strict'
 
 const jsonQuery = require('json-query');
 var AWS = require('aws-sdk');
+
+/**
+ * Define AWS API version
+ */
 
 AWS.config.apiVersions = {
   cloudformation: '2010-05-15',
@@ -11,225 +20,93 @@ AWS.config.apiVersions = {
 var cloudformation = new AWS.CloudFormation();
 var codepipeline = new AWS.CodePipeline();
 var apigateway = new AWS.APIGateway();
+var lammbda = new AWS.Lambda();
 
+// Lambda handler start here.
 exports.handler = function(event, context, callback) {
 
-    var jobId = event["CodePipeline.job"].id;
-    // Retrieve the value of UserParameters from the Lambda action configuration in AWS CodePipeline, in this case a URL which will be
-    // health checked by this function.
-    var stackName = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters; 
-    //var stackName = event["CodePipeline.job"].data.inputArtifacts[0].name;
+    //Retrieve the CodePipeline ID 
 
-    // Retrieve the value of UserParameters from the Lambda action configuration in AWS CodePipeline, in this case a URL which will be
-    // health checked by this function.
-    // var stackParams = {
-    //     StackName: stackName,
-    //     TemplateStage: 'Processed'
-    // };
-    
+    /**
+     * Retrieve the value of UserParameters from the Lambda action configuration in AWS CodePipeline, in this case a URL which will be
+     * health checked by this function.
+     */
+    //var stackName = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters; 
+
+    // Define the Cloudformation stack parameters. The processed CF template need to be used.     
     var stackParams = {
-        StackName: stackName || '',
+        StackName: 'CICD-Test-Node-CFStackName',
         TemplateStage: 'Processed'
     };
 
+    // REST Api Id of the deployed API.
     var restApiIdVal;
+   
+    cloudformation.getTemplate(stackParams, function(err, data) {
+        if (err) { 
+            console.log(err, err.stack);
+        }
+        else {
+            console.log(data);
 
-    var putJobSuccess = function(message) {
-      
-        var cpParams = {
-            jobId: jobId
-        };
+            var templateBody = data.TemplateBody; // template body.
+            var jsonTemplate = JSON.parse(templateBody);
+            // Retreive the API Name
+            var restApiName = jsonTemplate.Resources.CCTApi.Properties.Name;
+            
+            // Define the API List parameters. 
+            var apiListParams = {
+                limit: 20,   
+            };
+            
+            // Retrieve All the API and then pass the Restapiid to retrieve the correct API.
+            apigateway.getRestApis(apiListParams, function(err, data) {
+                if (err) {
+                    //console.log(err, err.stack) 
+                }    
+                else {
+                    //console.log(data); 
+                    var currentApiData = jsonQuery('items[name=' + restApiName+ '].id', {
+                        data: data
+                    }) 
 
-        console.log("Job Id: ", jobId);
-        // console.log("Stack Name: ", stackName);
-        codepipeline.putJobSuccessResult(cpParams, function(err, data) {
-            if (err) {
-                callback(err);
-            }
-            else {
-                cloudformation.getTemplate(stackParams, function(err, data) {
-                    if (err) { 
-                        console.log(err, err.stack);
-                    }
-                    else {
-                        console.log(data);
-                        var templateBody = data.TemplateBody;
-                        var jsonTemplate = JSON.parse(templateBody);
-                        var restApiName = jsonTemplate.Resources.CCTApi.Properties.Name;
-                        var apiListParams = {
-                            limit: 20,   
-                        };
-                        
-                        apigateway.getRestApis(apiListParams, function(err, data) {
-                            if (err) {
-                                //console.log(err, err.stack) 
-                            }    
-                            else {
-                                //console.log(data); 
-                                var currentApiData = jsonQuery('items[name=' + restApiName+ '].id', {
-                                    data: data
-                                }) 
+                    restApiIdVal = currentApiData.value;
+                    console.log("Rest API Id: ", restApiIdVal);
+                    var apiStageParams = {
+                        restApiId: restApiIdVal /* required */
+                        //limit: 0,   
+                    };
 
-                                restApiIdVal = currentApiData.value;
-                                console.log("Rest API Id: ", restApiIdVal);
-                                var apiStageParams = {
-                                    restApiId: restApiIdVal /* required */
-                                    //limit: 0,   
-                                };
+                    apigateway.getStages(apiStageParams, function(err, data) {
+                        console.log("Stages: ", data);
+                        if (err) {
+                            console.log(err, err.stack)
+                        }    
+                        // an error occurred
+                        else {   
+                    
+                            //Staging Stage info.    
+                            var deploymentIdOfStaging = jsonQuery('item[stageName=staging].deploymentId', {
+                                data: data
+                            })
+                            var deploymentIdOfStagingVal = deploymentIdOfStaging.value;
 
-                                apigateway.getStages(apiStageParams, function(err, data) {
-                                    console.log("Stages: ", data);
-                                    if (err) {
-                                        console.log(err, err.stack)
-                                    }    
-                                    // an error occurred
-                                    else {   
-                                
-                                        //Stage stage info.
-                                        var deploymentIdOfStage = jsonQuery('item[stageName=Stage].deploymentId', {
-                                            data: data
-                                        })
-                                        var deploymentIdOfStageVal = deploymentIdOfStage.value;
-
-                                        //Dev Stage info.
-                                        var deploymentIdOfDev = jsonQuery('item[stageName=dev].deploymentId', {
-                                            data: data
-                                        })
-                                        var deploymentIdOfDevVal = deploymentIdOfDev.value;
-
-                                        //Staging Stage info.    
-                                        var deploymentIdOfStaging = jsonQuery('item[stageName=staging].deploymentId', {
-                                            data: data
-                                        })
-                                        var deploymentIdOfStagingVal = deploymentIdOfStaging.value;
-
-                                        //Production Stage info.
-                                        var deploymentIdOfProd = jsonQuery('item[stageName=prod].deploymentId', {
-                                            data: data
-                                        })
-                                        var deploymentIdOfProdVal = deploymentIdOfProd.value;
-
-                                        
-
-                                        // Define Stage Parameter list.    
-                                        // The default stage "Stage" creaetd by the SAM needs to be deleted.
-                                        // Two new stages need to be created as "staging" and "prod" respectively. 
-                                        
-                                        // Delete Param list of "Stage" stage.
-                                        var deleteStageParams = {
-                                            restApiId: restApiIdVal, /* required */
-                                            stageName: 'Stage' /* required */
-                                        };
-
-                                        // Delete Param list of "Stage" stage.
-                                        var deleteDevParams = {
-                                            restApiId: restApiIdVal, /* required */
-                                            stageName: 'dev' /* required */
-                                        };    
-
-                                        // Create Param list of "Staging" stage.
-                                        var createStagingStageParams = {
-                                            deploymentId: deploymentIdOfStageVal, /* required */
-                                            restApiId: restApiIdVal, /* required */
-                                            stageName: 'staging', /* required */
-                                            variables: {
-                                            'version': 'staging'
-                                            }
-                                        };
-
-                                        // Create Param list of "Prod" stage.    
-                                        var createProdStageParams = {
-                                            deploymentId: deploymentIdOfStageVal, /* required */
-                                            restApiId: restApiIdVal, /* required */
-                                            stageName: 'prod', /* required */
-                                            variables: {
-                                            'version': 'prod'
-                                            }
-                                        };
-
-                                        console.log("Staging Stage: ", deploymentIdOfStagingVal);
-                                        // Create "staging" Stage.
-                                        if (deploymentIdOfStagingVal === null) {
-                                            apigateway.createStage(createStagingStageParams, function(err, data) {
-                                                if (err) {
-                                                    console.log(err, err.stack); // an error occurred
-                                                }    
-                                                else{
-                                                    console.log(data);           // successful response 
-                                                }   
-                                            });
-                                        }     
-
-                                        if (deploymentIdOfDevVal != null) {
-                                            apigateway.deleteStage(deleteDevParams, function(err, data) {
-                                                if (err) {
-                                                    console.log(err, err.stack); // an error occurred
-                                                }    
-                                                else{
-                                                    console.log(data);           // successful response    
-                                                }   
-                                            }); 
-                                        }
-
-                                        console.log("Prod Stage: ", deploymentIdOfProdVal);
-                                        // Create "prod" Stage.
-                                        if (deploymentIdOfProdVal === null) {
-                                            apigateway.createStage(createProdStageParams, function(err, data) {
-                                                if (err) {
-                                                    console.log(err, err.stack); // an error occurred
-                                                }    
-                                                else{
-                                                    console.log(data);           // successful response 
-                                                }   
-                                            });
-                                        }    
-
-                                        // Delete the default stage "Stage" created by the build file.
-                                        // A wrong and error stuff created by the SAM template.
-                                        // We don't require it.
-                                        if (deploymentIdOfStageVal != null) {
-                                            apigateway.deleteStage(deleteStageParams, function(err, data) {
-                                                if (err) {
-                                                    console.log(err, err.stack); // an error occurred
-                                                }    
-                                                else{
-                                                    console.log(data);           // successful response    
-                                                }   
-                                            }); 
-                                        }
-                                       
-                                    }    
-                                // console.log("Stage Message: " + util.inspect(stagesStagePresent.value, {depth: null})); 
-                                });  
-                            }    
-                        });
-                    }
-                });
-                callback(null, message);
-            }    
-        });    
-    }    
-
-    // Notify AWS CodePipeline of a failed job
-    var putJobFailure = function(message) {
-        var params = {
-            jobId: jobId,
-            failureDetails: {
-                message: JSON.stringify(message),
-                type: 'JobFailed',
-                externalExecutionId: context.invokeid
-            }
-        };
-        codepipeline.putJobFailureResult(params, function(err, data) {
-            context.fail(message);      
-        });
-    };
-
-    // Validate the URL passed in UserParameters
-    if(!stackName) {
-        putJobFailure('The UserParameters field must contain the Stack Name!');  
-        return;
-    }
-
-    putJobSuccess('Success');
-};
+                            //Production Stage info.
+                            var deploymentIdOfProd = jsonQuery('item[stageName=prod].deploymentId', {
+                                data: data
+                            })
+                            var deploymentIdOfProdVal = deploymentIdOfProd.value;
+                            
+                            //Check for the Stage creation.
+                            if (deploymentIdOfProd && deploymentIdOfStagingVal) {
+                                context.succeed('Success');
+                            }
+                            context.fail('Failure');
+                        }    
+                    // console.log("Stage Message: " + util.inspect(stagesStagePresent.value, {depth: null})); 
+                    });  
+                }    
+            });
+        }
+    });            
+}
