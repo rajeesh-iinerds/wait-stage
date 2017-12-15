5a8b57987b1d18057c59ceaf9a99f3cc29744fbc
Rajeesh Koroth <rajeesh.k@iinerds.com>
2017-12-15 18:47:13 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 /**
  * @author Rajeesh <rajeesh.k@iinerds.com>
- * @version: 0.3
+ * @version: 0.2
  */
 
 'use strict'
 
-const jsonQuery = require('json-query');
-var AWS = require('aws-sdk');
-
 /**
  * Define AWS API version
  */
-
 AWS.config.apiVersions = {
   cloudformation: '2010-05-15',
   // other service API versions
 };
 
-var cloudformation = new AWS.CloudFormation();
-var codepipeline = new AWS.CodePipeline();
+/**
+ * Intiialize the required AWS API
+ */
+const jsonQuery = require('json-query');
+var AWS = require('aws-sdk');
 var apigateway = new AWS.APIGateway();
-var lammbda = new AWS.Lambda();
 
-// Lambda handler start here.
+// Lambda handler starts here.
 exports.handler = function(event, context, callback) {
 
     // API Id of the deployed API. Passed by the StepFunction!!
@@ -36,12 +34,12 @@ exports.handler = function(event, context, callback) {
      * This need to be automated basd on the config values.
      */
     apigateway.getStages(apiStageParams, function(err, data) {
+       // an error occurred
         if (err) {
             console.log(err, err.stack)
         }    
-        // an error occurred
+        
         else {   
-    
             //Staging Stage info.    
             var deploymentIdOfStaging = jsonQuery('item[stageName=staging].deploymentId', {
                 data: data
@@ -53,21 +51,32 @@ exports.handler = function(event, context, callback) {
                 data: data
             })
             var deploymentIdOfProdVal = deploymentIdOfProd.value;
+
+            //Production Stage info.
+            var deploymentIdOfDev = jsonQuery('item[stageName=dev].deploymentId', {
+                data: data
+            })
+            var deploymentIdOfDevVal = deploymentIdOfDev.value;
             
             /** Check for the Presence of Both the "staging" and "prod" stage.
-             * The value is "body is evaluted by the "
+             * The value in "body" is evaluted by the StepFunction: 
+             * arn:aws:states:us-east-2:902849442700:stateMachine:WaitStage
+             * Unless the both the "prod" and "staging" stages exists, this Lambda will be 
+             * called by StepFunction in an infinite loop on an interval of 5 seconds.
+             * Typically API stages takes maximum of 30 seconds to reflect the changes; 
+             * so an around of maximum of SIX loop calls to this function.
              */
-
-            if (deploymentIdOfProdVal && deploymentIdOfStagingVal) {
-                    callback(null, {
-                        statusCode: '200',
-                        body: 'success',
-                    });
+            if (deploymentIdOfProdVal && deploymentIdOfStagingVal && deploymentIdOfDevVal) {
+                callback(null, {
+                    statusCode: '200',
+                    body: 'success',
+                });
             }
             
+            // Unless both "staging" and "prod" stages are present.
             callback(null, {
-                        statusCode: '200',
-                        body: 'failure',
+                statusCode: '200',
+                body: 'failure',
             });
         }
     });            
